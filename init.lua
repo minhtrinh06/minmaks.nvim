130e36b088fb84d5d67cc0f1dde4c5d7918636c7
Minh Trinh <minhtritrinh06@gmail.com>
2026-05-05 11:17:05 +1000

    configure to treesitter main

--- init.lua
+++ init.lua
@@ -566,7 +566,7 @@ require('lazy').setup({
       -- NOTE: Add LSPs here -- lspa
       local servers = {
         -- gopls = {},
-        pyright = {},
+        -- pyright = {},
         -- yamlls = {},
         clangd = {},
         rust_analyzer = {},
@@ -867,6 +867,7 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     build = ':TSUpdate',
     main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -874,14 +875,8 @@ require('lazy').setup({
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
+      highlight = { enable = true },
+      indent = { enable = true },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
@@ -917,7 +912,7 @@ require('lazy').setup({
   require 'minh.plugins.ts-comments',
   require 'minh.plugins.marks',
   require 'minh.plugins.vim-visual-multi',
-  require 'minh.plugins.unrealengine',
+  -- require 'minh.plugins.unrealengine',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
